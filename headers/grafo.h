01a7f76705bdbce00f4a71bb53215a91b7d7118b
Matheus <matheuspd07@gmail.com>
2022-12-06 16:22:16 -0300

    Started TAD grafo

--- headers/grafo.h
+++ headers/grafo.h
@@ -1,6 +1,37 @@
 #ifndef __GRAFO__
 #define __GRAFO__
 
+#include "registros.h"
 
+typedef struct aresta {
+    int idPoPsConectado;
+    float velocidade;
+    Aresta *prox;
+    size_t size;
+    size_t numLigacoes;
+} Aresta;
+
+typedef struct vertice {
+    int id;
+    int numVerticesLigados;
+    char nomePoPs[TAM_MAX_NOME];
+    char nomePais[TAM_MAX_NOME];
+    char siglaPais[3];
+    Aresta *primeiro;
+} Vertice;
+
+typedef struct grafo {
+    int totalVertices;
+    int totalArestas;
+    Vertice *vertices;
+    size_t sizeGrafo;
+} Grafo;
+
+Grafo *criaGrafo();
+void liberaGrafo(Grafo *gr);
+int insereGrafo(Grafo *gr, REGISTRO reg);
+int insereElementoOrdenado(Grafo *gr, Vertice *vertice, int pos, REGISTRO reg);
+int existeVertice(Grafo *gr, int id);
+int existeAresta(Grafo *gr, int id1, int id2);
 
 #endif
--- src/grafo.c
+++ src/grafo.c
@@ -1,2 +1,47 @@
 #include "grafo.h"
 
+Grafo *criaGrafo() {    // Cria grafo vazio
+    Grafo *gr = (Grafo*)malloc(sizeof(Grafo));
+    if(gr != NULL) {
+        gr->totalArestas = 0;
+        gr->totalVertices = 0;
+        gr->sizeGrafo = 100;
+        gr->vertices = malloc(sizeof(Vertice)*gr->sizeGrafo);
+        if(gr->vertices == NULL) {
+            free(gr);
+            return NULL;
+        }
+    }
+    return gr;
+}
+
+void liberaGrafo(Grafo *gr) {
+    if(gr != NULL) {
+        
+    }
+}
+
+int insereGrafo(Grafo *gr, REGISTRO reg) {
+    if(gr != NULL) {
+        if(gr->totalVertices == 0) {  // Grafo vazio
+            
+        }
+        else {            
+            
+        }
+    }
+    return 0;
+}
+
+int insereElementoOrdenado(Grafo *gr, Vertice *vertice, int pos, REGISTRO reg) {
+    
+
+}
+
+int existeVertice(Grafo *gr, int id) {
+
+}
+
+int existeAresta(Grafo *gr, int id1, int id2) {
+
+}
